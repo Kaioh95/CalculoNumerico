d21eaf2c31f19373bd718920698575f03db2c306
Kaio Henrique <kaioh95@gmail.com>
2020-10-04 16:00:46 -0300

    Tarefa3 completa

--- Tarefa3/gauss.py
+++ Tarefa3/gauss.py
@@ -32,9 +32,9 @@ class EliminacaoGauss:
 
 	def set_b(self):
 		print("*********************************************************");
-		print("Vetor b:");
+		#print("Vetor b:");
 		[self.b.append(linha[-1]) for linha in self.matriz];
-		print(self.b);
+		#print(self.b);
 
 	def eliminacao(self, N, parcial):
 		for passo in range(N-1):
@@ -59,11 +59,11 @@ class EliminacaoGauss:
 				for j_op in range(N+1):
 					self.matrizR[ipasso][j_op] = self.matrizR[ipasso][j_op] - m*self.matrizR[passo][j_op];
 
-		print("\n Matriz apos a eliminacao: ");
-		for i in range(self.N):
-			for j in range(self.N+1):
-				print(self.matrizR[i][j], end = " ");
-			print();
+		#print("\n Matriz apos a eliminacao: ");
+		#for i in range(self.N):
+		#	for j in range(self.N+1):
+		#		print(self.matrizR[i][j], end = " ");
+		#	print();
 		self.calcular_x();
 
 	def calcular_x(self):
@@ -78,12 +78,11 @@ class EliminacaoGauss:
 			xi = (self.matrizR[i][self.N] - soma)/self.matrizR[i][i];
 			self.x[i] = xi;
 
-		print("\nVetor x:");
-		print(self.x);
+		#print("\nVetor x:");
+		#print(self.x);
 		self.calcularVetorResiduo();
  
 	def calcularVetorResiduo(self):
-		print("\nVetor Residuo: ");
 		soma = 0;
 
 		for i in range(self.N):
@@ -93,13 +92,15 @@ class EliminacaoGauss:
 			soma = 0;
 			self.residuo.append(self.b[i] - self.Ax[i]);
 
-		print(self.residuo);
-		print("*********************************************************\n\n");
+		#print("\nVetor Residuo: ");
+		#print(self.residuo);
+		#print("*********************************************************\n\n");
 
 	def calcularNorma(self):
 		soma = math.fsum([math.pow(valor, 2) for valor in self.residuo]);
-		print("Norma do vetor residual: ");
+		print("\nNorma do vetor residual: ");
 		print(math.sqrt(soma));
+		print("*********************************************************\n\n");
 
 	def resetMatriz(self):
 		self.matriz = [];
@@ -112,7 +113,7 @@ class EliminacaoGauss:
 
 
 
-def refinamento(elGauss, totalIteracoes):
+def refinamento(elGauss, totalIteracoes, pivotamento):
 	r = copy.deepcopy(elGauss.residuo);
 	n = elGauss.N;
 	elGauss.resetMatriz();
@@ -124,25 +125,49 @@ def refinamento(elGauss, totalIteracoes):
 		for i in range(n):
 			elGauss.matriz[i][-1] = r[i];
 			elGauss.matrizR[i][-1] = r[i];
-		elGauss.eliminacao(elGauss.N, 1);
+		elGauss.set_b();
+		elGauss.eliminacao(elGauss.N, pivotamento);
 		
 		elGauss.calcularNorma();
 		r = copy.deepcopy(elGauss.residuo);
 		n = elGauss.N;
 		elGauss.resetMatriz();
 
-#m1 = EliminacaoGauss("m1.in");
-#m1.eliminacao(m1.N, 0);
-#m1.resetMatriz();
-#m1.eliminacao(m1.N, 1);
+print("Arquivo m1");
+m1 = EliminacaoGauss("m1.in");
+print("Sem pivotamento parcial: ");
+m1.eliminacao(m1.N, 0);
+m1.calcularNorma();
+#refinamento(m1, 150, 0);
 
+m1.resetMatriz();
+print("Com pivotamento parcial: ");
+m1.eliminacao(m1.N, 1);
+m1.calcularNorma();
+#refinamento(m1, 150, 1);
+
+print("Arquivo m2");
 m2 = EliminacaoGauss("m2.in");
-#m2.eliminacao(m2.N, 0);
-#m2.resetMatriz();
-m2.eliminacao(m2.N, 1);
-refinamento(m2, 150);
+print("Sem pivotamento parcial: ");
+m2.eliminacao(m2.N, 0);
+m2.calcularNorma();
+#refinamento(m2, 150, 0);
 
-#m3 = EliminacaoGauss("m3.in");
-#m3.eliminacao(m3.N, 0);
-#m3.resetMatriz();
-#m3.eliminacao(m3.N, 1);
+m2.resetMatriz();
+print("Com pivotamento parcial: ");
+m2.eliminacao(m2.N, 1);
+m2.calcularNorma();
+#refinamento(m2, 150, 1);
+
+print("Arquivo m3");
+m3 = EliminacaoGauss("m3.in");
+print("Sem pivotamento parcial: ");
+m3.eliminacao(m3.N, 0);
+m3.calcularNorma();
+#refinamento(m3, 100, 0);
+
+m3.resetMatriz();
+print("Com pivotamento parcial: ");
+m3.eliminacao(m3.N, 1);
+m3.calcularNorma();
+#refinamento(m3, 100, 1);
